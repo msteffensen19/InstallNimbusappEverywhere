d9e953d31745b78262e73a8fe77a5d0b592c5b38
Mark Steffensen <mark.steffensen@microfocus.com>
2021-12-14 14:26:07 -0700

    Update InstallNimbusapp.bat

--- Windows/InstallNimbusapp.bat
+++ Windows/InstallNimbusapp.bat
@@ -1,2 +1,9 @@
+# Nimbusapp installation on Windows
+# Requires docker to already be install in C:\Programs\Docker
+
+echo "---Backing up existing nimbusapp.pl file into nimbusapp.pl.orig"
 xcopy /y "C:\Program Files\Docker\nimbusapp.pl C:\Program Files\Docker\nimbusapp.pl.orig"
+
+echo "---Copying in new nimbusapp.pl file..."
 xcopy /y ..\nimbusapp_1.6.0_ms  "C:\Program Files\Docker\nimbusapp.pl"
+echo "---Done!"
