496902fa475e46a10a20be15ba307238d54db866
Mark Steffensen <mark.steffensen@microfocus.com>
2021-12-14 19:27:14 +0000

    Fixed DOS format

--- Linux/InstallNimbusapp.sh
+++ Linux/InstallNimbusapp.sh
@@ -3,7 +3,7 @@
 # Make a copy of it for restore purposes first
 
 echo "Backing up the current nimbusapp command to nimbusapp.orig..."
-cp /usr/local/bin/nimbusapp /usr/local/bin/nimbusapp.orig
+sudo cp /usr/local/bin/nimbusapp /usr/local/bin/nimbusapp.orig
 
 echo "Copying new nimbusapp command to /usr/local/bin"
 dos2unix ../nimbusapp_1.6_ms
