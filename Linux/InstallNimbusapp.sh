2d145278a4968dee5b09dd3569bb65146448575a
Mark Steffensen <mark.steffensen@microfocus.com>
2021-12-14 12:11:48 -0700

    Update InstallNimbusapp.sh

--- Linux/InstallNimbusapp.sh
+++ Linux/InstallNimbusapp.sh
@@ -4,7 +4,7 @@
 
 cp /usr/local/bin/nimbusapp /usr/local/bin/nimbusapp.orig
 
-dos2unix ./nimbusapp_1.6_ms.txt
-sudo cp  ./nimbusapp_1.6_ms.txt /usr/local/bin/nimbusapp
+dos2unix ../nimbusapp_1.6_ms.txt
+sudo cp  ../nimbusapp_1.6_ms.txt /usr/local/bin/nimbusapp
 
 sudo chmod +x /usr/local/bin/nimbusapp
