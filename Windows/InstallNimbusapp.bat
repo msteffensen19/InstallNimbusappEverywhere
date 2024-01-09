1b28e6e3d258d64042d431a9f31b6c12f60a3c01
Mark Steffensen <msteffensen19@gmail.com>
2024-01-09 13:00:46 -0700

    Update InstallNimbusapp.bat

--- Windows/InstallNimbusapp.bat
+++ Windows/InstallNimbusapp.bat
@@ -3,6 +3,13 @@
 :: Requires docker to already be install in C:\Programs\Docker
 :: Requires Strawberry Perl v5.20.0 or greater
 
+echo "---Downloading a compatible version of Strawberry Perl (175 MB)..."
+curl https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_5380_5361/strawberry-perl-5.38.0.1-64bit.msi -o strawberry-perl-5.38.0.1-64bit.msi
+echo "---Follow the prompts and allow Perl to be installed..."
+
+echo "---Installing Perl 5.38.0.1 - Approve and follow the prompts"
+./strawberry-perl-5.38.0.1-64bit.msi
+
 echo ---Backing up existing nimbusapp.pl file into nimbusapp.pl.orig (if it exists)
 echo f | xcopy /f /y "C:\Program Files\Docker\nimbusapp.pl" "C:\Program Files\Docker\nimbusapp.pl.orig"
 
