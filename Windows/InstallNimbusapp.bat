a56cc15b49ed5deca2c6bf139036e4bfd987a819
Mark Steffensen <msteffensen19@gmail.com>
2024-03-05 09:12:51 -0700

    Update InstallNimbusapp.bat

--- Windows/InstallNimbusapp.bat
+++ Windows/InstallNimbusapp.bat
@@ -7,7 +7,7 @@ echo "---Downloading a compatible version of Strawberry Perl (5.38.2.2 - 175 MB)
 echo "---This can take a minute or so."
 curl.exe -OL https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit.msi
 
-echo "---Installing Perl 5.38.2.2 - Approve and follow the prompts to install Perl to C:\strawberry"
+echo "---Installing Perl 5.38.2.2 - Approve and follow the prompts to install Perl to C:\Strawberry"
 .\strawberry-perl-5.38.2.2-64bit.msi
 
 echo "---Removing Strawberry Perl MSI file..."
